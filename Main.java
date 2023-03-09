c82eab928c4a5a4fa95864ad04a4f38f3f453bbc
Jakub Feliks <maxmaster12@o2.pl>
2023-03-09 15:43:20 +0000

    C1

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Podaj liczbę 1: ");
-    int a = scanner.nextInt();
-    System.out.println("Podaj liczbę 2: ");
-    int b = scanner.nextInt();
-    System.out.println("Suma: " + (a + b));
+    Scanner skaner = new Scanner(System.in);
+    System.out.println("podaj liczbę a:");
+    int a = skaner.nextInt();
+    System.out.println("podaj liczbę b:");
+    int b = skaner.nextInt();
+    int wynik = a + b;
+    System.out.println("Wynik: " + wynik);    
   }
 }
